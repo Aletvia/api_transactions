a65b636358648765a8e972a14b846a17cbfd94dd
Aletvia Lecona <aletvialecona@gmail.com>
2021-11-20 01:25:56 -0600

    additional reports

--- api/urls/companies.py
+++ api/urls/companies.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from api.views.v_companies import CompanyResumView
+from api.views.v_companies import CompanyResumView, CompanyVSCompanyApprovedResumView, CompanyVSCompanyRejectedResumView
 
 urlpatterns = [
    path('resume/<int:code>',
          CompanyResumView.as_view(),
          name='company_resume_view'),
+   path('storm/approved',
+         CompanyVSCompanyApprovedResumView.as_view(),
+         name='storm_approved_view'),
+   path('storm/rejected',
+         CompanyVSCompanyRejectedResumView.as_view(),
+         name='storm_rejected_view'),
 ]
--- api/views/v_companies.py
+++ api/views/v_companies.py
@@ -9,3 +9,21 @@ class CompanyResumView(APIView):
 
     def get(self, request, code):
         return Resume().per_company(code)
+
+
+class CompanyVSCompanyApprovedResumView(APIView):
+    """
+        View dedicated to return a resume from the transactions approved by company
+    """
+
+    def get(self, request):
+        return Resume().company_vs_company_transactions_approved()
+
+
+class CompanyVSCompanyRejectedResumView(APIView):
+    """
+        View dedicated to return a resume from the transactions rejected by company
+    """
+
+    def get(self, request):
+        return Resume().company_vs_company_transactions_rejected()
